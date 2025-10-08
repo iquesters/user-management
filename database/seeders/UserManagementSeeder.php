963422ecb5350842bb6b9b94785ccd3feb07baf8
Pritam Bag <pritam02b@gmail.com>
2025-10-08 09:59:46 +0530

    enhancement: updated seeder

--- database/seeders/UserManagementSeeder.php
+++ database/seeders/UserManagementSeeder.php
@@ -9,6 +9,23 @@ class UserManagementSeeder extends BaseModuleSeeder
     protected string $moduleName = 'user-management';
     protected string $description = 'user-management module';
     protected array $metas = [
-        'module_icon' => 'fa-solid fa-box'
+        'module_icon' => 'fas fa-user-gear',
+        'module_sidebar_menu' => [
+            [
+                "icon" => "fas fa-users",
+                "label" => "Users",
+                "route" => "users.index",
+            ],
+            [
+                "icon" => "fas fa-user-shield",
+                "label" => "Roles",
+                "route" => "roles.index",
+            ],
+            [
+                "icon" => "fas fa-shield-alt",
+                "label" => "Permissions",
+                "route" => "permissions.index",
+            ]
+        ]
     ];
 }
